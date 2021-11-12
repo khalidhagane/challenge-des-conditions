089a3940a42a28184963cf985975bc2a4c3d7cc1
unknown <khalidhagane62@gmail.com>
2021-11-12 20:33:59 +0100

    challenge2

---
+++ challenge 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int m;
+
+    printf("entrer le nombre M ");
+    scanf("%d",&m);
+
+    if(m%2==0){
+        printf(" le nombre est paire ");
+        }
+    else
+        printf("le nombre est impaire ");
+
+
+
+}
