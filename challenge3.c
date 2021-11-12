c733805245dc27edff71c10f9fa0577fae7f90d6
unknown <khalidhagane62@gmail.com>
2021-11-12 23:01:25 +0100

    challenge 3

---
+++ challenge3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char alpha;
+    printf("entrer un caracter : ");
+    scanf("%c",&alpha);
+
+    switch(alpha){
+
+        case 'a' :
+        printf("caractere voyelle");
+        break;
+         case 'e' :
+        printf("caractere voyelle");
+        break;
+         case 'i' :
+        printf("caractere voyelle");
+        break;
+         case 'o' :
+        printf("caractere voyelle");
+        break;
+         case 'u' :
+        printf("caractere voyelle");
+        break;
+         case 'y' :
+        printf("caractere voyelle");
+        break;
+        case 'A' :
+        printf("caractere voyelle");
+        break;
+         case 'E' :
+        printf("caractere voyelle");
+        break;
+         case 'I' :
+        printf("caractere voyelle");
+        break;
+         case 'O' :
+        printf("caractere voyelle");
+        break;
+         case 'U' :
+        printf("caractere voyelle");
+        break;
+         case 'Y' :
+        printf("caractere voyelle");
+        break;
+        default :
+                printf("caractere non voyelle");
+
+}
+    return 0;
+}
